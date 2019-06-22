50c690388757c0ad91c2cd4947ccb2d50e5297e2
Davidbillingsley <48254616+Davidbillingsley@users.noreply.github.com>
2019-06-21 22:11:06 -0700

    Fixed dual windows
    Made small changes to the design of the code, removed the dual instances,  Calling tk.Toplevel() generates a new top window to use.

--- video.py
+++ video.py
@@ -8,10 +8,8 @@ install openCV first : sudo apt-get install libopencv-dev python-opencv
 """
 
 import cv2
-import Tkinter as tk
-from Tkinter import *
-import ttk
-from ttk import Frame
+import tkinter as tk
+from tkinter import *
 import Image, ImageTk
 
 white 		= "#ffffff"
@@ -28,7 +26,8 @@ mainWindow.geometry('%dx%d+%d+%d' % (maxWidth,maxHeight,0,0))
 mainWindow.resizable(0,0)
 # mainWindow.overrideredirect(1)
 
-mainFrame = Frame(mainWindow)
+#Sets the Frame in the App of MainWindow
+mainFrame = tk.Frame(mainWindow)
 mainFrame.place(x=20, y=20)                
 
 #Capture video frames
@@ -48,9 +47,9 @@ def show_frame():
 	lmain.configure(image=imgtk)
 	lmain.after(10, show_frame)
 
-closeButton = Button(mainWindow, text = "CLOSE", font = fontButtons, bg = white, width = 20, height= 1)
-closeButton.configure(command= lambda: mainWindow.destroy())              
-closeButton.place(x=270,y=430)	
+closeButton = tk.Button(mainWindow, text = "CLOSE", font = fontButtons, bg = white, width = 20, height= 1,
+						command= lambda: mainWindow.destroy())
+closeButton.place(x=270, y=430)
 
 show_frame()  #Display
-mainWindow.mainloop()  #Starts GUI
+mainWindow.mainloop()  #Loops for Windows
